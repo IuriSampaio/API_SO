e11bde7866d72474b5a42330c315dc16498cae1c
iuriSampaio <iurisampaio18@gmail.com>
2021-04-18 14:20:08 -0300

    adding cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import allowMethods from './middleware/allowMethods';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(allowMethods);
+app.use(cors());
 
 app.use(routes);
 
